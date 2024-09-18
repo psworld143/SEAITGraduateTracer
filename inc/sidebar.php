102073b20a61a4dc183eea637eeccf93a0c7d1b3
Angelo Salem <angelosalem173.30@gmail.com>
2024-09-18 08:40:44 +0800

    tracer

--- inc/sidebar.php
+++ inc/sidebar.php
@@ -1,45 +1,50 @@
-  <!-- ======= Sidebar ======= -->
-  <aside id="sidebar" class="sidebar">
+<!-- ======= Sidebar ======= -->
+<aside id="sidebar" class="sidebar">
 
+    <!-- Avatar Section -->
     <div class="sidebar-avatar">
-      <img src="assets/img/image.jpg" alt="User Avatar" class="avatar-img">
+        <img src="assets/img/image.png" alt="User Avatar" class="avatar-img">
     </div>
     <br>
 
+    <!-- Sidebar Navigation -->
     <ul class="sidebar-nav" id="sidebar-nav">
 
-      <li class="nav-item">
-        <a class="nav-link " href="index.php">
-          <i class="bi bi-grid"></i>
-          <span>Dashboard</span>
-        </a>
-      </li><!-- End Dashboard Nav -->
-      
-      <li class="nav-heading">Survey</li>
+        <!-- Dashboard -->
+        <li class="nav-item">
+            <a class="nav-link active" href="index.php">
+                <i class="bi bi-grid"></i>
+                <span>Dashboard</span>
+            </a>
+        </li><!-- End Dashboard Nav -->
 
-      <li class="nav-item">
-        <a class="nav-link collapsed" href="manage_survey.php">
-          <i class="bi bi-file-text"></i>
-          <span>Manage Survey</span>
-        </a>
-      </li>
+        <!-- Survey Section -->
+        <li class="nav-heading">Survey</li>
 
-      <li class="nav-item">
-        <a class="nav-link collapsed" href="survey_result.php">
-          <i class="bi bi-graph-up"></i>
-          <span>Survey Result</span>
-        </a>
-      </li>
+        <li class="nav-item">
+            <a class="nav-link collapsed" href="manage_survey.php">
+                <i class="bi bi-file-text"></i>
+                <span>Manage Survey</span>
+            </a>
+        </li>
 
-      <li class="nav-heading">User Management</li>
+        <li class="nav-item">
+            <a class="nav-link collapsed" href="survey_result.php">
+                <i class="bi bi-graph-up"></i>
+                <span>Survey Result</span>
+            </a>
+        </li>
 
-      <li class="nav-item">
-        <a class="nav-link collapsed" href="users_profile.php">
-          <i class="bi bi-person-circle"></i>
-          <span>User Profile</span>
-        </a>
-      </li>
+        <!-- User Management Section -->
+        <li class="nav-heading">User Management</li>
 
-    </ul>
+        <li class="nav-item">
+            <a class="nav-link collapsed" href="users_profile.php">
+                <i class="bi bi-person-circle"></i>
+                <span>User Profile</span>
+            </a>
+        </li>
 
-  </aside><!-- End Sidebar-->
+    </ul><!-- End Sidebar Navigation -->
+
+</aside><!-- End Sidebar -->
